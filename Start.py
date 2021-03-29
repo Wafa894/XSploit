d5ce0ea467c510050bb8c7d7c61c7b3a4e0e7594
Wafa894 <81066048+Wafa894@users.noreply.github.com>
2021-03-29 03:05:16 +0000

    Update Start.py

--- Start.py
+++ Start.py
@@ -48,7 +48,7 @@ def menu():
     if pil =="1":
 	os.system("pkg install unstable-repo")
 	os.system("pkg install nmap")
-	os.system("python2 Bernyanyi.py")
+	os.system("python2 Start.py")
     elif pil =="2":
 	os.system("pkg update && pkg upgrade")
 	os.system("pkg install python -y")
@@ -60,7 +60,7 @@ def menu():
 	os.system("bash m-wiz.sh")
 	os.system("cd")
 	os.system("msfconsole")
-	os.system("python2 Bernyanyi.py")
+	os.system("python2 Start.py")
     elif pil =="3":
 	os.system("apt update")
 	os.system("apt upgrade")
@@ -74,7 +74,7 @@ def menu():
 	os.system("cd wifi")
 	os.system("chmod +x wifi.sh")
 	os.system("sh wifi.sh")
-	os.system("python2 Bernyanyi.py")
+	os.system("python2 Start.py")
     elif pil =="0":
         sys.exit()
 menu()
